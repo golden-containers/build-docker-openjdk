3bf7965d1b3b7146a460be49115ae863328fefe0
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 13:35:39 -0800

    OpenJDK initial

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ cd working
 
 # Checkout upstream
 
-# git clone --depth 1 --branch main https://github.com/upstream/upstream.git
-# cd upstream
+git clone --depth 1 --branch master https://github.com/docker-library/openjdk
+cd openjdk
 
 # Transform
 
-# sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye/" 17/bullseye/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/" 18/jdk/bullseye/Dockerfile
 
 # Build
 
-# docker build --tag ghcr.io/golden-containers/upstream-bullseye-slim:17 17/bullseye-slim
+docker build --tag ghcr.io/golden-containers/openjdk:18-bullseye 18/jdk/bullseye
 
 # Push
 
-# docker push ghcr.io/golden-containers/upstream-bullseye-slim:17
+docker push ghcr.io/golden-containers/openjdk -a
