5bf4db5867ab3591a7001896e992df310b3af5c4
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 11:14:57 -0800

    Add gsed for macOS

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ cd openjdk
 
 # Transform
 
+# This sed syntax is GNU sed specific
+# [ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
+
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/" 18/jdk/bullseye/Dockerfile
 
 # Build
